fb34c7f4a41662264a3fd3f3b23d59916832418f
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-06 22:35:28 -0300

    calculation of distances have been completed

--- js/slide.js
+++ js/slide.js
@@ -1,35 +1,52 @@
 export default class Slide {
-    constructor(slide, wrapper) {
-        this.slide = document.querySelector(slide)
-        this.wrapper = document.querySelector(wrapper)
-    }
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+  }
 
-    onStart(event) {
-        event.preventDefault()
-        this.wrapper.addEventListener('mousemove', this.onMove)
-    }
+  moveSlide(distX) {
+    this.dist.movedPosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
 
-    onMove() {
-    }
+  upDatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
+  }
 
-    onEnd() {
-        this.wrapper.removeEventListener('mousemove', this.onMove)
-    }
+  onStart(event) {
+    event.preventDefault();
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
 
-    addSlideEvents() {
-        this.wrapper.addEventListener('mousedown', this.onStart)
-        this.wrapper.addEventListener('mouseup', this.onEnd)
-    }
+  onMove(event) {
+    const finalPostion = this.upDatePosition(event.clientX);
+    this.moveSlide(finalPostion);
+  }
 
-    bindEvents() {
-        this.onStart = this.onStart.bind(this)
-        this.onMove = this.onMove.bind(this)
-        this.onEnd = this.onEnd.bind(this)
-    }
+  onEnd() {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movedPosition;
+ }
 
-    init() {
-        this.bindEvents()
-        this.addSlideEvents()
-        return this
-    }
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.upDatePosition = this.upDatePosition.bind(this);
+    this.moveSlide = this.moveSlide.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
 }
